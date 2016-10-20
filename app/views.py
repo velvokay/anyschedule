7d70e75f68a273077bb0f66a4dc1725843e66623
Alex Yakovlev <alexakovlev@gmail.com>
2016-10-20 09:39:35 -0700

    Templating for future views.

--- app/views.py
+++ app/views.py
@@ -5,6 +5,20 @@ app = Flask(__name__)
 @app.route('/')
 def index():
   return render_template('index.html')
- 
+  
+@app.route('/register')
+def register():
+	
+	return render_template('register.html')
+	
+@app.route('/login')
+def login():
+
+	return render_template('login.html')
+	
+@app.route('/dashboard')
+def dashboard():
+
+	return render_template('dashboard.html')
 #if __name__ == '__main__':
 #  app.run(debug=True)
