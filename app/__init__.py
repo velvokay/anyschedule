4980d452741ede6bc21df1cc89a7831e3491e49a
Alex Yakovlev <alexakovlev@gmail.com>
2017-01-08 23:25:07 -0800

    setting up __init__

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
 
-app = Flask(__name__)
-app.secret_key = 'alpine'
+#app.secret_key = 'alpine'
 
-import app.views
+#from app import views
--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
-from app import app
+ 
+app = Flask(__name__)      
  
 @app.route('/')
 def index():
