2a1c28d60e674b4e8f8752777205ca1575d8bde3
Alex Yakovlev <alexakovlev@gmail.com>
2017-01-09 13:29:11 -0800

    established Item class in db model.

--- app/models.py
+++ app/models.py
@@ -3,3 +3,17 @@ from werkzeug import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
+class Item(db.Model):
+	__table__name = 'Item'
+	id = db.Column(db.Integer, primary_key = True)
+	name = db.Column(db.String(45))
+	price = db.Column(db.String(10))
+	quantity = db.Column(db.Integer)
+	description = db.Column(db.String(5000))
+	date = db.Column(db.String(10))
+	
+	def __init__(self, name, price, description, date):
+		self.name = name.title()
+		self.price = price.title()
+		self.description = description.title()
+		self.date = date.title()
