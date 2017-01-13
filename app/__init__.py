e1ac83c4ca128f598ea82a8aa161e8f724469a32
Alex Yakovlev <alexakovlev@gmail.com>
2017-01-12 22:32:57 -0800

    emptied init file for now.

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
-from flask import Flask
-from views import app
+#from flask import Flask
+#from app.views import app
 
-app.secret_key = 'alpine'
+#app.secret_key = 'alpine'
 
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://jtcpp:alpine64@mysql.server/jtcpp$scanwdb'
 
